c76bcbde46fea0206d34cf0e02e96fdde2c6218c
Harshil Shah <hkshah1978@gmail.com>
2024-04-23 19:30:11 +0530

    "Modified the files"

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ app.config['MYSQL_DB'] = 'electoral_bonds'
 
 mysql = MySQL(app)
 
-def get_data():
+
+@app.route('/', methods = ["POST", "GET"])
+def main_page():
     cursor=mysql.connection.cursor()
     cursor.execute('select distinct name_of_purchaser from purchase_details')
     name_of_purchaser=list(cursor.fetchall())
@@ -21,12 +23,8 @@ def get_data():
     name_of_party=list(cursor.fetchall())
     name_of_party.sort()
     cursor.close()
-    return name_of_purchaser,name_of_party
-
-@app.route('/', methods = ["POST", "GET"])
-def main_page():
-    name_of_purchaser,name_of_party=get_data()
     return render_template("index.html",name_of_purchaser=name_of_purchaser,name_of_party=name_of_party)
+
 bond_number=0
 @app.route('/a_2', methods = ["POST", "GET"])
 def a_2():
@@ -37,9 +35,9 @@ def a_2():
         data=cursor.fetchall()
         bond_number=request.form['box']
     if len(data)==0:
-        return render_template('index.html',a_2_data = [['Not Found']])
+        return render_template('e_1.html',a_2_data = [['Not Found']])
     print(data)
-    return render_template("e_1.html", a_2_data = data,filter=['Name of Purchaser','Name of Party'],bond_number=bond_number,table_name=table_name) 
+    return render_template("e_1.html", a_2_data = data,bond_number=bond_number,table_name=table_name) 
 @app.route('/e_1',methods=['POST','GET'])
 def e_1():
     if request.method == "POST":
@@ -77,13 +75,12 @@ def e_2():
             c+=1
         cursor.close()
     if len(data)==0:
-        return render_template('index.html',e_2_data = [['Not Found']])
+        return render_template('e_2.html',e_2_data = [['Not Found']])
     print(d)
     years=list(d.keys())
     amount=list(d.values())
     number=[c]
     print(amount,number)
-    name_of_purchaser,name_of_party=get_data()
     return render_template('e_2.html',e_2_data = data,years=years,amount=amount,company=value,number=number)
         
 @app.route('/e_3',methods=['POST','GET'])
@@ -103,9 +100,8 @@ def e_3():
     years=list(d.keys())
     amount=list(d.values())
     if len(data)==0:
-        return render_template('index.html',e_3_data = [['Not Found']])
-    name_of_purchaser,name_of_party=get_data()
-    return render_template('e_3.html',e_3_data = data,years1=years,amount1=amount)
+        return render_template('e_3.html',e_3_data = [['Not Found']])
+    return render_template('e_3.html',e_3_data = data,years1=years,amount1=amount,party=request.form['party'])
 
 @app.route('/e_4',methods=['POST','GET'])
 def e_4():
@@ -115,8 +111,7 @@ def e_4():
         data=cursor.fetchall()
         print(data)
         if len(data)==0:
-            return render_template('index.html',e_4_data = [['Not Found']])
-        name_of_purchaser,name_of_party=get_data()
+            return render_template('e_4.html',e_4_data = [['Not Found']])
         d={}
         for i in range(len(data)):
             d[data[i][1]]=d.get(data[i][1],0)+data[i][2]
@@ -132,14 +127,13 @@ def e_5():
         cursor.execute('select name_of_party,denominations from redemption_details where bond_number in (select bond_number from purchase_details where name_of_purchaser = %s)',(request.form['company'],))
         data=cursor.fetchall()
         if len(data)==0:
-            return render_template('index.html',e_5_data = [['Not Found']])
+            return render_template('e_5.html',e_5_data = [['Not Found']])
         d={}
         for i in range(len(data)):
             d[data[i][0]]=d.get(data[i][0],0)+data[i][1]
         parties=list(d.keys())
         total_amount=list(d.values())
         total_denominations=sum(total_amount)
-        name_of_purchaser,name_of_party=get_data()
         return render_template('e_5.html',e_5_data = list(d.items()),parties=parties,total_amount=total_amount,total_denominations=total_denominations,company=request.form['company'])
 
 @app.route('/e_6',methods=['POST','GET'])
@@ -156,5 +150,6 @@ def e_6():
         party=list(d.keys())
         total_donations=list(d.values())
     return render_template('e_6.html',party=party,total_donations=total_donations)
+
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port="80", debug = True) 
